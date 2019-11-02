f0faf8f3b2a0e28b95d961d26644165b82aec7b3
Wenda Ni <wenni@microsoft.com>
2019-11-02 22:15:40 +0000

    Add unit test for show interfaces status / intfutil status
    Signed-off-by: Wenda Ni <wenni@microsoft.com>

---
+++ sonic-utilities-tests/intfutil_test.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from click.testing import CliRunner
+from unittest import TestCase
+import subprocess
+
+import show.main as show
+
+root_path = os.path.dirname(os.path.abspath(__file__))
+modules_path = os.path.dirname(root_path)
+scripts_path = os.path.join(modules_path, "scripts")
+
+class TestIntfutil(TestCase):
+    @classmethod
+    def setup_class(cls):
+        print("SETUP")
+        os.environ["PATH"] += os.pathsep + scripts_path
+        os.environ["UTILITIES_UNIT_TESTING"] = "1"
+
+    def setUp(self):
+        self.runner = CliRunner()
+
+    # Test 'show interfaces status' / 'intfutil status'
+    def test_intf_status(self):
+        # Test 'show interfaces status'
+        result = self.runner.invoke(show.cli.commands["interfaces"].commands["status"], [])
+        print >> sys.stderr, result.output
+        expected_output = (
+              "Interface    Lanes    Speed    MTU      Alias    Vlan    Oper    Admin             Type    Asym PFC\n"
+            "-----------  -------  -------  -----  ---------  ------  ------  -------  ---------------  ----------\n"
+            "  Ethernet0        0      25G   9100  Ethernet0  routed    down       up  QSFP28 or later         off"
+        )
+        self.assertEqual(result.output.strip(), expected_output)
+
+        # Test 'intfutil status'
+        output = subprocess.check_output('intfutil status', stderr=subprocess.STDOUT, shell=True)
+        print >> sys.stderr, output
+        self.assertEqual(output.strip(), expected_output)
+        assert(0)
+
+    def test_intf_status_verbose(self):
+        result = self.runner.invoke(show.cli.commands["interfaces"].commands["status"], ["--verbose"])
+
+    @classmethod
+    def teardown_class(cls):
+        print("TEARDOWN")
+        os.environ["PATH"] = os.pathsep.join(os.environ["PATH"].split(os.pathsep)[:-1])
+        os.environ["UTILITIES_UNIT_TESTING"] = "0"
