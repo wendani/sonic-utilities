972e0f5af1f7ec3d03af59cffb52dc9c5d95e80a
Wenda Ni <wenni@microsoft.com>
2019-11-04 02:28:48 +0000

    Add unit test for showing status of single sub interface in alias naming mode
    Signed-off-by: Wenda Ni <wenni@microsoft.com>

--- sonic-utilities-tests/intfutil_test.py
+++ sonic-utilities-tests/intfutil_test.py
@@ -20,6 +20,7 @@ class TestIntfutil(TestCase):
     def setUp(self):
         self.runner = CliRunner()
 
+
     # Test 'show interfaces status' / 'intfutil status'
     def test_intf_status(self):
         # Test 'show interfaces status'
@@ -44,6 +45,7 @@ class TestIntfutil(TestCase):
         expected_output = "Command: intfutil status"
         self.assertEqual(result.output.split('\n')[0], expected_output)
 
+
     # Test 'show subinterfaces status' / 'intfutil status subport'
     def test_subintf_status(self):
         # Test 'show subinterfaces status'
@@ -54,6 +56,7 @@ class TestIntfutil(TestCase):
           "--------------------  -------  -----  ------  -------  --------------------\n"
           "        Ethernet0.10      25G   9100      10       up  802.1q-encapsulation"
         )
+        self.assertEqual(result.output.strip(), expected_output)
 
         # Test 'intfutil status subport'
         output = subprocess.check_output('intfutil status subport', stderr=subprocess.STDOUT, shell=True)
@@ -67,8 +70,10 @@ class TestIntfutil(TestCase):
         expected_output = "Command: intfutil status subport"
         self.assertEqual(result.output.split('\n')[0], expected_output)
 
+
     # Test single sub interface status
     def test_single_subintf_status(self):
+        # Test 'show subinterfaces status Ethernet0.10'
         result = self.runner.invoke(show.cli.commands["subinterfaces"].commands["status"], ["Ethernet0.10"])
         print >> sys.stderr, result.output
         expected_output = (
@@ -76,7 +81,9 @@ class TestIntfutil(TestCase):
           "--------------------  -------  -----  ------  -------  --------------------\n"
           "        Ethernet0.10      25G   9100      10       up  802.1q-encapsulation"
         )
+        self.assertEqual(result.output.strip(), expected_output)
 
+        # Test 'intfutil status Ethernet0.10'
         output = subprocess.check_output('intfutil status Ethernet0.10', stderr=subprocess.STDOUT, shell=True)
         print >> sys.stderr, output
         self.assertEqual(output.strip(), expected_output)
@@ -88,7 +95,22 @@ class TestIntfutil(TestCase):
         expected_output = "Command: intfutil status Ethernet0.10"
         self.assertEqual(result.output.split('\n')[0], expected_output)
 
+
+    # Test alias mode of single sub interface status
+    def test_single_subintf_status_alias_mode(self):
+        os.environ["SONIC_CLI_IFACE_MODE"] = "alias"
+        result = self.runner.invoke(show.cli.commands["subinterfaces"].commands["status"], ["etp1.10"])
+        print >> sys.stderr, result.output
+        expected_output = (
+            "Sub port interface    Speed    MTU    Vlan    Admin                  Type\n"
+          "--------------------  -------  -----  ------  -------  --------------------\n"
+          "        Ethernet0.10      25G   9100      10       up  802.1q-encapsulation"
+        )
+        self.assertEqual(result.output.strip(), expected_output)
+        os.environ["SONIC_CLI_IFACE_MODE"] = "default"
+
         assert(0)
+    # Test '--verbose' alias mode of single sub interface status
 
     @classmethod
     def teardown_class(cls):
